5f8557646fad4eddb054dbc169573b4ad0114ae0
Nguyen Thanh Nguyen <ntnguyencse@gmail.com>
2023-07-17 14:02:55 +0000

    Add cilium helm v1.13.0
    Signed-off-by: Nguyen Thanh Nguyen <ntnguyencse@gmail.com>

--- main.go
+++ main.go
@@ -12,9 +12,16 @@ func main() {
 	valueFile := "/home/ubuntu/ntnguyen-helm/helm/test/values.yaml"
 	chartName := "chartname"
 	helmArgs := []string{"install", "-f", valueFile, chartName, calicoChartPackagedPath, "--kubeconfig", kubePath, "--dry-run", "--debug", "--v", "5"}
-	err := helmwrapper.ApplyHelmWrapper(kubePath, calicoChartPackagedPath, true, true, helmArgs, []string{})
-	if err != nil {
-		fmt.Println("error:", err)
-	}
 
+	err := helmwrapper.ApplyHelmWrapper(kubePath, calicoChartPackagedPath, true, true, helmArgs, []string{})
+
+	if err != nil {
+		fmt.Println("error: 1", err)
+	}
+	ciliumPath := "/home/ubuntu/install-deps/cilium/install/kubernetes/cilium/cilium-1.13.0.tar.gz"
+	ciliumHelmArgs := []string{"install", chartName, ciliumPath, "--kubeconfig", kubePath, "--dry-run", "--debug", "--v", "5"}
+	err = helmwrapper.ApplyHelmWrapper(kubePath, ciliumPath, true, true, ciliumHelmArgs, []string{})
+	if err != nil {
+		fmt.Println("error: 2", err)
+	}
 }
