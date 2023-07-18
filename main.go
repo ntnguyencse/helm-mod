d8cfac2b8df9cb8cf182b40a70c8d08957487736
Nguyen Thanh Nguyen <ntnguyencse@gmail.com>
2023-07-18 07:52:08 +0000

    Install prometheus test
    Signed-off-by: Nguyen Thanh Nguyen <ntnguyencse@gmail.com>

--- main.go
+++ main.go
@@ -7,20 +7,21 @@ import (
 )
 
 func main() {
-	kubePath := "/home/ubuntu/config"
-	calicoChartPackagedPath := "https://github.com/jenkinsci/helm-charts/releases/download/jenkins-4.4.1/jenkins-4.4.1.tgz"
-	valueFile := "/home/ubuntu/ntnguyen-helm/helm/test/values.yaml"
-	chartName := "chartname"
-	helmArgs := []string{"install", "-f", valueFile, chartName, calicoChartPackagedPath, "--kubeconfig", kubePath, "--dry-run", "--debug", "--v", "5"}
+	kubePath := "/home/ubuntu/ntnguyen-helm/helm/test/kubeconfig/cluster-test"
+	// calicoChartPackagedPath := "https://github.com/jenkinsci/helm-charts/releases/download/jenkins-4.4.1/jenkins-4.4.1.tgz"
+	// valueFile := "/home/ubuntu/ntnguyen-helm/helm/test/values.yaml"
+	// chartName1 := "chartname1"
+	// helmArgs := []string{"install", "-f", valueFile, chartName1, calicoChartPackagedPath, "--kubeconfig", kubePath, "--debug", "--v", "5"}
 
-	err := helmwrapper.ApplyHelmWrapper(kubePath, calicoChartPackagedPath, true, true, helmArgs, []string{})
+	// err := helmwrapper.ApplyHelmWrapper(kubePath, calicoChartPackagedPath, true, true, helmArgs, []string{})
 
-	if err != nil {
-		fmt.Println("error: 1", err)
-	}
-	ciliumPath := "/home/ubuntu/install-deps/cilium/install/kubernetes/cilium/cilium-1.13.0.tar.gz"
-	ciliumHelmArgs := []string{"install", chartName, ciliumPath, "--kubeconfig", kubePath, "--dry-run", "--debug", "--v", "5"}
-	err = helmwrapper.ApplyHelmWrapper(kubePath, ciliumPath, true, true, ciliumHelmArgs, []string{})
+	// if err != nil {
+	// 	fmt.Println("error: 1", err)
+	// }
+	chartName2 := "chartname21"
+	ciliumPath := "https://github.com/prometheus-community/helm-charts/releases/download/kube-prometheus-stack-46.7.0/kube-prometheus-stack-46.7.0.tgz"
+	ciliumHelmArgs := []string{"install", chartName2, ciliumPath, "--kubeconfig", kubePath, "--debug", "--v", "5"}
+	err := helmwrapper.ApplyHelmWrapper(kubePath, ciliumPath, true, true, ciliumHelmArgs, []string{})
 	if err != nil {
 		fmt.Println("error: 2", err)
 	}
