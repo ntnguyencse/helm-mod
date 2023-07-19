933c9e36b18b7e8bc82418cda0b5c6e673fb9eb0
Nguyen Thanh Nguyen <ntnguyencse@gmail.com>
2023-07-19 05:09:48 +0000

    Remove comment code
    Signed-off-by: Nguyen Thanh Nguyen <ntnguyencse@gmail.com>

--- main.go
+++ main.go
@@ -8,16 +8,6 @@ import (
 
 func main() {
 	kubePath := "/home/ubuntu/ntnguyen-helm/helm/test/kubeconfig/cluster-test"
-	// calicoChartPackagedPath := "https://github.com/jenkinsci/helm-charts/releases/download/jenkins-4.4.1/jenkins-4.4.1.tgz"
-	// valueFile := "/home/ubuntu/ntnguyen-helm/helm/test/values.yaml"
-	// chartName1 := "chartname1"
-	// helmArgs := []string{"install", "-f", valueFile, chartName1, calicoChartPackagedPath, "--kubeconfig", kubePath, "--debug", "--v", "5"}
-
-	// err := helmwrapper.ApplyHelmWrapper(kubePath, calicoChartPackagedPath, true, true, helmArgs, []string{})
-
-	// if err != nil {
-	// 	fmt.Println("error: 1", err)
-	// }
 	chartName2 := "chartname21"
 	ciliumPath := "https://github.com/prometheus-community/helm-charts/releases/download/kube-prometheus-stack-46.7.0/kube-prometheus-stack-46.7.0.tgz"
 	ciliumHelmArgs := []string{"install", chartName2, ciliumPath, "--kubeconfig", kubePath, "--debug", "--v", "5"}
