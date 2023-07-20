0d396d6860725da685c14da8521a2781c9d56cff
Nguyen Thanh Nguyen <ntnguyencse@gmail.com>
2023-07-20 14:25:45 +0000

    Update charts
    Signed-off-by: Nguyen Thanh Nguyen <ntnguyencse@gmail.com>

--- main.go
+++ main.go
@@ -8,9 +8,9 @@ import (
 
 func main() {
 	kubePath := "/home/ubuntu/ntnguyen-helm/helm/test/kubeconfig/cluster-test"
-	chartName2 := "chartname21"
-	ciliumPath := "https://github.com/prometheus-community/helm-charts/releases/download/kube-prometheus-stack-46.7.0/kube-prometheus-stack-46.7.0.tgz"
-	ciliumHelmArgs := []string{"install", chartName2, ciliumPath, "--kubeconfig", kubePath, "--debug", "--v", "5"}
+	chartName2 := "cilium"
+	ciliumPath := "https://github.com/ntnguyencse/helm-mod/raw/main/test/charts/cilium-1.13.0.tar.gz"
+	ciliumHelmArgs := []string{"install", chartName2, ciliumPath, "--namespace", "cilium-system", "--kubeconfig", kubePath}
 	err := helmwrapper.ApplyHelmWrapper(kubePath, ciliumPath, true, true, ciliumHelmArgs, []string{})
 	if err != nil {
 		fmt.Println("error: 2", err)
